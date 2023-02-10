435952cea5da4f2e76ac33c24d5530edfe621864
vishnu <vishnuvardhan.u@westagilelabs.com>
2023-02-10 09:11:35 +0530

    .

--- set.js
+++ set.js
@@ -4,9 +4,8 @@ set.add({a:10})
 set.add({b:5})
 set.add({c:2})
 
-
+set.add(5)
 console.log(set);
-set.delete(5)
 
 console.log(set);
 
@@ -14,3 +13,11 @@ set.forEach(element =>{
   if(element.b == 5) set.delete(element)
 })
 console.log(set);
+
+console.log(set.has(5));
+console.log(
+  set.values()
+);
+console.log(set.keys());
+
+console.log(set.entries());
